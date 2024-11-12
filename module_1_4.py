671043643fb62531cf83a02132b6535da164a2cc
djonkovalev <djon9489@gmail.com>
2024-11-12 20:57:46 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,12 @@
+my_string = input('How was your day?: ')
+print (my_string [0 :])
+print(my_string .upper())
+print(my_string .lower())
+print(my_string .replace(" ",""))
+print("my_string"[0])
+print("my_string"[-1])
+
+
+
+
+
